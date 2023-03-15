ebb9c2312601fc9e5846d68171aba46a391acc3a
lobobeast <101597902+lobobeast@users.noreply.github.com>
2023-03-14 20:47:02 -0400

    align naming

--- src/routes/contracts.route.js
+++ src/routes/contracts.route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const appController = require('../controllers/app.controller');
+const contractsController = require('../controllers/contracts.controller');
 
-router.post('/get-abi', appController.getAbi);
-router.post('/get-sc', appController.getSC);
+router.post('/get-abi', contractsController.getAbi);
+router.post('/get-sc', contractsController.getSC);
 
 module.exports = router;
